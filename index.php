4706f99d63491eaa47dff1bb75277f5370616258
miplayer1 <mhiezely@hotmail.fr>
2018-10-13 18:06:52 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,366 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>Cookies | La recette ultime
+    </title>
+    <meta name="keywords" content="cookies, recette cookies" />
+    <meta name="publisher" content="cooldev">
+    <meta name="description" content="Après avoir cherché et testé plusieurs recettes de cookies, voici l'ultime recette, celle qui marche, qui est bonne et qu'on peut décliner à l'infini !" />
+    <meta name="viewport" content="width=device-width initial-scale=1 user-scalable=no" />
+    <meta name="google-site-verification" content="rBRnpj5XSqUqYyI27OQ52hRYjk1PkW2Z1i3E9ces7oE" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css" />
+    <link rel="stylesheet" href="css/main.css" />
+    <link rel="Shortcut icon" href="favicon.ico" type="image/x-icon" />
+    <!-- Open Graph Meta Tags -->
+    <meta property="og:title" content="Cookies | La recette ultime" />
+    <meta property="og:site_name" content="La recette ultime des cookies">
+    <meta property="og:url" content="http://cookies.cooldev.xyz" />
+    <meta property="og:description" content="Après avoir cherché et testé plusieurs recettes de cookies, voici l'ultime recette, celle qui marche, qui est bonne et qu'on peut décliner à l'infini !" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="http://cookies.mpaw.xyz/assets/img/og-img.png" />
+    <!-- Piwik -->
+    <!-- End Piwik Code -->
+  </head>
+  <body >
+    <div class="navbar-fixed">
+      <nav>
+        <div class="nav-wrapper brown darken-3">
+          <a href="#!" class="center brand-logo">
+            <img src="assets/img/logo-cookie.png" alt="logo cookie">
+          </a>
+          <a href="#" data-activates="mobile-menu" class="button-collapse right">
+            <i class="material-icons">menu
+            </i>
+          </a>
+          <ul class="right hide-on-med-and-down">
+            <li>
+              <a href="#section1">Ingrédients
+              </a>
+            </li>
+            <li>
+              <a href="#section2">Recette
+              </a>
+            </li>
+          </ul>
+          <ul class="side-nav" id="mobile-menu">
+            <li>
+              <a href="#section1">Ingrédients
+              </a>
+            </li>
+            <li>
+              <a href="#section2">Recette
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <!--NAV-->
+    <header>
+      <div class="header-content">
+        <div class="row">
+          <div class="col s12 center-align">
+            <img class="responsive-img" src="assets/img/logo-texte-cookie.png" alt="Logo" width="340px">
+          </div>
+          <!--<div class="col s12 ">
+<h1 class="center-align white-text">Tagline</h1>-->
+          <div class="separator col s4 offset-s4 divider">
+          </div>
+        </div>
+        <div class="col s12 social center-align">
+          <a href="https://www.facebook.com/#" target="_blank">
+            <img class="responsive-img" src="assets/img/logo-fb.png" alt="Lien vers facebook">
+          </a>
+          <a href="https://twitter.com/#" target="_blank">
+            <img class="responsive-img" src="assets/img/logo-twitter.png" alt="Lien vers twitter">
+          </a>
+          <a href="https://instagram.com/#" target="_blank">
+            <img class="responsive-img" src="assets/img/insta.png" alt="Lien vers insta">
+          </a>
+        </div>
+      </div>
+      </div>
+    <div class="overlay">
+    </div>
+    </header>
+  <!--HEADER-->
+  <section id="section1" class="scrollspy">
+    <div class="row">
+      <div class="col s12 m8 offset-m2 ">
+        <h2 class="center-align grey-text">Ingrédients : 
+        </h2>
+      </div>
+      <div class="separator col s2 offset-s5 divider">
+      </div>
+    </div>
+    <!--TITLE-->
+    <div class="row">
+      <div id="cards" class="col s10 offset-s1 m8 offset-m2">
+        <div class="col s12 m6 l4 ">
+          <div class="card">
+            <div class="card-image orange lighten-2 waves-effect waves-block waves-light">
+              <img class="activator" src="assets/img/farine.png" alt="">
+            </div>
+            <div class="card-content">
+              <h3 class="card-title activator">220g de farine
+                <i class="material-icons right">add
+                </i>
+              </h3>
+            </div>
+            <!--CARD-CONTENT-->
+            <div class="card-reveal">
+              <h4 class="card-title">Variante
+                <i class="material-icons right">close
+                </i>
+              </h4>
+              <p class="flow-text">Farine type 55, type 80...Remplacez une partie de la farine par de la noix de coco rapée. Avec le chocolat blanc, c'est top !
+              </p>
+            </div>
+            <!--CARD-REVEAL-->
+          </div>
+        </div>
+        <!--CARD 1-->
+        <div class="col s12 m6 l4 ">
+          <div class="card">
+            <div class="card-image teal lighten-3 card-image waves-effect waves-block waves-light">
+              <img  class="activator" src="assets/img/oeuf.png" alt="">
+            </div>
+            <div class="card-content">
+              <h3 class="card-title activator">1 oeuf
+              </h3>
+            </div>
+            <!--CARD-CONTENT-->     
+          </div>
+        </div>
+        <!--CARD 2-->
+        <div class="col s12 m6 l4 ">
+          <div class="card">
+            <div class="card-image  red accent-2 waves-effect waves-block waves-light">
+              <img class="activator" src="assets/img/beurre.png" alt="">
+            </div>
+            <div class="card-content">
+              <h3 class="card-title activator">100g de beurre ramolli
+                <i class="material-icons right ">add
+                </i>
+              </h3>
+            </div>
+            <!--CARD-CONTENT-->
+            <div class="card-reveal">
+              <h4 class="card-title">Variante
+                <i class="material-icons right">close
+                </i>
+              </h4>
+              <p class="flow-text">On a testé avec du beurre de cacahuète et de l'huile de coco (même quantité)
+              </p>              
+            </div>
+            <!--CARD-REVEAL-->
+          </div>
+        </div>
+        <!--CARD 3-->
+        <div class="col s12 m6 l4 ">
+          <div class="card">
+            <div class="card-image orange lighten-2 waves-effect waves-block waves-light">
+              <img class="activator" src="assets/img/chocolat.png" alt="">
+            </div>
+            <div class="card-content">
+              <h3 class="card-title activator">100g de chocolat
+                <i class="material-icons right">add
+                </i>
+              </h3>
+            </div>
+            <!--CARD-CONTENT-->
+            <div class="card-reveal">
+              <h4 class="card-title">Variantes
+                <i class="material-icons right">close
+                </i>
+              </h4>
+              <p class="flow-text">Chocolat noir, chocolat au lait, chocolat blanc, chocolat à l'orange... Ajouté des noix de macadamia ou des noix de pécan
+              </p>
+            </div>
+            <!--CARD-REVEAL-->
+          </div>
+        </div>
+        <!--CARD 4-->
+        <div class="col s12 m6 l4 ">
+          <div class="card">
+            <div class="card-image teal lighten-3 card-image waves-effect waves-block waves-light">
+              <img  class="activator" src="assets/img/bicarbonate.png" alt="">
+            </div>
+            <div class="card-content">
+              <h3 class="card-title activator">1/2 cuillère à café de bicarbonate
+              </h3>
+            </div>
+            <!--CARD-CONTENT-->
+          </div>
+        </div>
+        <!--CARD 5-->
+        <div class="col s12 m6 l4 ">
+          <div class="card">
+            <div class="card-image  red accent-2 waves-effect waves-block waves-light">
+              <img class="activator" src="assets/img/sucre.png" alt="">
+            </div>
+            <div class="card-content">
+              <h3 class="card-title activator">75g de sucre roux + 75g de cassonade
+                <i class="material-icons right ">add
+                </i>
+              </h3>
+            </div>
+            <!--CARD-CONTENT-->
+            <div class="card-reveal">
+              <h4 class="card-title">Variantes
+                <i class="material-icons right">close
+                </i>
+              </h4>
+              <p class="flow-text">150g de sucre roux, ou 150g de sucre blanc
+              </p>              
+            </div>
+            <!--CARD-REVEAL-->
+          </div>
+        </div>
+        <!--CARD 6-->
+      </div>
+    </div>
+    <!--CARDS-->
+  </section>
+  <!--SECTION 1-->
+  <section id="section2" class="brown darken-3 scrollspy">	
+    <div class="container">
+      <div class="row">
+        <div class="col s12 m8 offset-m2 white-text">
+          <h2 class="center-align">Recette : 
+          </h2>
+        </div>
+        <div class="separator col s2 offset-s5 divider">
+        </div>
+        <div class="col s12 ">
+          <img class="responsive-img" src="assets/img/ustencils.png" alt=""> 
+          <ol class="flow-text white-text ">
+            <li>Préparer les ustensiles (couteau, cuillère, saladier, verre doseur ou balance, papier sulfurisé et plaque allant au four) et les ingrédients, sortir le beurre du frigo et préchauffer le four à 180°C
+            </li> 
+            <li>Mettre dans le saladier la farine + le sucre + le bicarbonate et mélanger
+            </li> 
+            <li>Découper le beurre relativement mou en petits dés
+            </li>  
+            <li>Incorporer le beurre aux ingrédients du saladier en frottant avec les mains. Le résultat doit ressembler à du sable 
+            <li>Découper le chocolat en morceaux plus ou moins généreux et incorporer les pépites au mélange
+            </li>
+            <li>Il est possible de mettre du beurre fondu mais pas trop chaud pour un resultat plus rapide (mais en rognant sur la qualité finale du cookies, quand même ;)) en inversant l'instruction 4 avec la 5 	
+            <li>Faire un puit au milieu du mélange et y mettre un oeuf entier
+            </li>
+            <li>Commencer par mélanger à l'aide d'une cuillère puis malaxer avec les mains propres jusqu'a l'obtention d'une boule de pate homogène
+            </li>
+          </ol>
+        </div>
+        <div class="separator col s2 offset-s5 divider">
+        </div>
+        <div class="col s12">
+          <ul class="flow-text white-text center-align">
+            <li>Mettre du papier sulfirisé sur une plaque allant au four
+            </li>
+            <br>
+            <li>Prélever des petites boules de pates de même taille, les disposer sur la plaque et les écraser un peu pour former un cookie.
+              <br> 
+              <br>Il est possible de réaliser un cookies géant en faisant cela avec la boule de pâte entière et en rajoutant 4 minutes de cuisson.
+            </li>
+          </ul>
+        </div>
+        <div class="separator col s2 offset-s5 divider">
+        </div>
+        <div class="col s12 flow-text white-text center-align">
+          Mettre au four à 180°C pendant 10 minutes.
+          <br>
+          <br> ATTENTION : quand vous sortirez les cookies du four, vous allez avoir l'impression qu'ils ne sont pas cuits mais que neni ! En refroidissant, ils deviendront plus ferme et bien moelleux.
+        </div>
+      </div>
+      <div class="row center-align">
+        <div class="col s4 offset-s4">
+          <a href="recette-cookies-imprimable.php" target="_blank" class="waves-effect waves-light btn">
+            <i class="material-icons">&#xE8AD;</i>
+          </a>
+        </div>   
+      </div>
+    </div>
+  </section>
+  <!--SECTION 2-->
+  <section class="container">
+  
+    <div class="row " id="instafeed"><h3 class="grey-text valign-wrapper"><img src="assets/src/logo-small.png" alt=""> <a href="https://www.instagram.com/i_love_cookies_time/"style="color: grey;">@i_love_cookies_time</a></h3></br>
+    </div>
+  </section>
+  <footer class="page-footer brown darken-3">
+    <div class="container">
+      <div class="row ">
+        <div class="center-align social col s12">
+          <div class="col s12 social center-align">
+            <a href="https://www.facebook.com/#" target="_blank">
+              <img class="responsive-img" src="assets/img/logo-fb.png" alt="Lien vers facebook">
+            </a>
+            <a href="https://twitter.com/#" target="_blank">
+              <img class="responsive-img" src="assets/img/logo-twitter.png" alt="Lien vers twitter">
+            </a>
+            <a href="https://www.instagram.com/i_love_cookies_time/" target="_blank">
+              <img class="responsive-img" src="assets/img/insta.png" alt="Lien vers insta">
+            </a>
+          </div>
+        </div>
+        <!--SOCIAL-->
+      </div>
+    </div>
+    <div class="footer-copyright grey-text ">
+      <div class="container ">
+        <span class="white-text">CC BY-NC-SA 4.0 - 2017 /
+        </span> 
+        <a class="white-text" href="http://www.cooldev.xyz" target="_blank">La Team Cooldev
+        </a>
+        <a class="modal-trigger right white-text" href="#modal1" >Mentions légales
+        </a>
+        <div id="modal1" class="modal modal-fixed-footer">
+          <div class="modal-content">
+            <h3>Responsable de la publication :
+            </h3> 
+            <p>Mylène Hiezely
+            </a>
+          </p>
+        <h3>Hébergement :
+        </h3>
+        <p>
+          <a href="https://gandi.net">GANDI SAS
+          </a>, Société par Actions Simplifiée au capital de 300.000€ ayant son siège social au 63-65 boulevard Massena à Paris (75013) FRANCE, immatriculée sous le numéro 423 093 459 RCS PARIS N° TVA FR81423093459 Téléphone : +33.(0) 1 70.37.76.61 .
+        </p>
+        <h3>Gestion des données personnelles :
+        </h3>
+        <p>Votre vie privée est très importante pour nous. Pour connaître le trafic sur ce site internet, Nous utilisons 
+          <a href="https://piwik.org">Piwik
+          </a>. Piwik est une solution d’analytics qui respecte votre vie privée conformément aux recommandations de la CNIL et ne demande aucune déclaration préalable.
+        </p>
+      </div>
+      <div class="modal-footer ">
+        <a href="#!" class="modal-action modal-close btn-floating ">
+          <i class="material-icons ">highlight_off
+          </i>
+        </a>
+      </div>
+    </div>
+    <!--MODAL-->
+    </div>
+  </div>
+<!--COPYRIGHT-->
+</footer>
+<!--FOOTER-->
+<script
+        src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+        crossorigin="anonymous">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js" >
+</script>
+<script type="text/javascript" src="js/instafeed.min.js">
+</script>   
+<script type="text/javascript" src="js/custom.js">
+</script>   
+<script type="text/javascript" src="js/main.js">
+</script>
+</body>
+</html>
